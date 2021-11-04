37a6d6c7a459a302c8e8dedf1b897bb719319903
isabelaJs <isabelajimenezs96@gmail.com>
2021-11-04 17:51:03 -0500

    remove themeprovider

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
-import theme from './App/theme';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-
 
 
 ReactDOM.render(
 
-  <ThemeProvider theme={theme}>
+  <>
     <CssBaseline/>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </ThemeProvider>,
+  </>,
 
   document.getElementById('root')
 );
