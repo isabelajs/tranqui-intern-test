e64cbb901ab1598e7772e0e25431826cd714007b
isabelaJs <isabelajimenezs96@gmail.com>
2021-11-05 20:28:19 -0500

    add configuration to heroku deploy

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,11 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.static(path.join(path.dirname(__dirname), 'build')));
+
+
+console.log(path.join(path.dirname(__dirname), 'build'))
 
 //importo e inyecto las rutas
 app.use(router);
@@ -20,8 +24,10 @@ app.get("/api", (req, response) => {
   return response.json({ version: 0.1 });
 });
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//para toda que no coincida con las otras rutas
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(path.dirname(__dirname), 'build', 'index.html'));
 });
 
 app.listen(port, () => {
