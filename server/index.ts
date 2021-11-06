ed2d8f3b748fec9bdeec3a72c78102fa555fbf14
isabelaJs <isabelajimenezs96@gmail.com>
2021-11-06 12:56:49 -0500

    test only deploy api

--- server/index.ts
+++ server/index.ts
@@ -14,11 +14,14 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.use(express.static(path.join(path.dirname(__dirname), 'build')));
-
+// // /static/bundle.js
+// app.use(express.static(path.join(path.dirname(__dirname), 'build')));
 
 console.log(path.join(path.dirname(__dirname), 'build'))
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(path.dirname(__dirname), 'build', 'index.html'));
+// });
 
 //importo e inyecto las rutas
 app.use(router);
@@ -28,11 +31,6 @@ app.get("/api", (req, response) => {
 });
 
 //para toda que no coincida con las otras rutas
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(path.dirname(__dirname), 'build', 'index.html'));
-});
-
 app.listen(port, () => {
   console.log(`server started at ${port}`);
 });
